24992859e81a1c159b49f1418ef8f3ed69a8a249
Youssef Bouhjira <b.youssef91@gmail.com>
2013-05-16 16:13:27 +0100

    Removed gridster library

--- views-view-kanban.tpl.php
+++ views-view-kanban.tpl.php
@@ -1,7 +1,33 @@
 
 <? $index = 0; ?>
-<? foreach ($rows as $key => $row) : ?>
-    <li data-row="<? echo $index++ + 1; ?>" data-col="<? echo $column + 1; ?>" data-sizex="1" data-sizey="1">
-        <? echo ($column + 1) . ' | ' .($index); ?>
-    </li>
-<? endforeach; ?>
+<ul class="views-kanban-column">
+    <a href="#" class="button-fold button" title="fold"><<</a>
+    <h2><? echo $title; ?> </h2>
+    <? foreach ($rows as $key => $row) : ?>
+        <li class="views-kanban-card">
+            <? foreach ($row as $key => $field) : ?>
+                <? echo $row->{$key}; ?><br/>
+            <? endforeach; ?>
+        </li>
+    <? endforeach; ?>
+</ul>
+
+<script>
+    (function($) {
+        Drupal.behaviors.views_kanban_style = {
+            attach: function() {
+                var maxHeight = 0;
+                var cols = $('.views-kanban-column').each(function() {
+                    var h = $(this).outerHeight(true);
+                    console.log(h);
+                    if (h > maxHeight)
+                        maxHeight = h;
+                });
+                cols.css('height', maxHeight + 'px');
+                $(cols[0]).closest('.views-kanban').css({
+                    'height': (maxHeight + 20) + 'px',
+                });
+            }
+        };
+    })(jQuery);
+</script>
