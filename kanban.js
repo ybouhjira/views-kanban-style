b024cc93162572708e8b528933fb9d2d0d759281
Youssef Bouhjira <b.youssef91@gmail.com>
2013-05-18 22:15:55 +0100

    bug fix

--- kanban.js
+++ kanban.js
@@ -1,25 +1,25 @@
 (function($) {
-        Drupal.behaviors.views_kanban_style = {
-            attach: function() {
-                // Same height hack
-                var maxHeight = 0;
-                var cols = $('.views-kanban-column-wrapper').each(function() {
-                    var h = $(this).outerHeight(true);
-                    if (h > maxHeight)
-                        maxHeight = h;
+    Drupal.behaviors.views_kanban_style = {
+        attach: function() {
+            // Same height hack
+            var maxHeight = 0;
+            var cols = $('.views-kanban-column-wrapper').each(function() {
+                var h = $(this).outerHeight(true);
+                if (h > maxHeight)
+                    maxHeight = h;
+            });
+            cols.css('height', maxHeight + 'px');
+            $(cols[0]).closest('.views-kanban').css({
+                'height': (maxHeight + 20) + 'px'
+            });
+
+            // making the card draggable and sortable using jQueryUI
+            $('.views-kanban-column').each(function() {
+                $(this).sortable({
+                    connectWith: '.' + $(this).attr('class').match(/(?=\s*)views-col-(.*)(?=\s*)/)[0],
+                    placeholder: 'views-kanban-placeholder'
                 });
-                cols.css('height', maxHeight + 'px');
-                $(cols[0]).closest('.views-kanban').css({
-                    'height': (maxHeight + 20) + 'px',
-                });
-                
-                // making the card draggable and sortable using jQueryUI
-                $('.views-kanban-column').each(function(){
-                    $(this).sortable({
-                        connectWith: '.' + $(this).attr('class').match(/(?=\s*)views-col-(.*)(?=\s*)/)[0],
-                        placeholder: 'views-kanban-placeholder'
-                    });
-                });
-            }
-        };
-    })(jQuery);
+            });
+        }
+    };
+})(jQuery);
